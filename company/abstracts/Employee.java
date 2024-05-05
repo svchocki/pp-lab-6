211b9de071c3b8d9bfb7f4459fe76ed2c0ca9935
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-05 11:36:05 +0200

    Update Employee.java

--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -2,11 +2,15 @@ public abstract class Employee implements Employable {
     private String name;
     private double salary;
     private int id;
+    private String hireDate;
+    private String position;
 
-    public Employee(String name, double salary, int id) {
+    public Employee(String name, double salary, int id, String hireDate, String position) {
         this.name = name;
         this.salary = salary;
         this.id = id;
+        this.hireDate = hireDate;
+        this.position = position;
     }
 
     public String getName() {
@@ -15,5 +19,18 @@ public abstract class Employee implements Employable {
 
     public double getSalary() {
         return salary;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
