63cddddb0abaa50f2d14428576a6b8ac5f9b4b1d
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-05 11:39:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,25 +9,19 @@ public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("John", 2000.0, 1, "2022-01-01", "Junior Developer");
         Worker worker2 = new Worker("Alice", 2200.0, 2, "2021-12-15", "Senior Developer");
-        Worker worker3 = new Worker("Bob", 1900.0, 3, "2023-03-20", "Intern");
-        Worker worker4 = new Worker("Eve", 2100.0, 4, "2020-09-10", "Project Manager");
-
+        Worker worker3 = new Worker("Bob", 1900.0, 1, "2023-03-20", "Intern");
         Manager manager = new Manager("Michael", 5000.0, 5, "2019-05-01", "Head of Department");
 
-        List<Employee> employees = new ArrayList<>();
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-        employees.add(worker1);
-        employees.add(worker2);
-        employees.add(worker3);
-        employees.add(worker4);
-        employees.add(manager);
-
-        for (Employee employee : employees) {
-            employee.work();
-            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() +
-                    ", Position: " + employee.getPosition() +
-                    ", Hire date: " + employee.getHireDate() +
-                    ", Salary: " + employee.getSalary() + ")");
+        Worker workerToCompare = worker1;
+        for (Employee employee : List.of(worker1, worker2, worker3, manager)) {
+            if (employee.equals(workerToCompare)) {
+                System.out.println(workerToCompare.getName() + " is equal to " + employee.getName());
+            }
         }
     }
 }
