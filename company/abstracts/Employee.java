c27cc43fb02ab7dd29b3469cba881034763bed07
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-05 11:38:47 +0200

    Update Employee.java

--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -33,4 +33,16 @@ public abstract class Employee implements Employable {
     public int hashCode() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) obj;
+        return id == employee.id;
+    }
 }
